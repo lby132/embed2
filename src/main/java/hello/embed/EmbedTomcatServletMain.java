b3c4728f8e34b2b4e942b8acaa916ac850635c01
lby132 <quddbscjswow@naver.com>
2024-02-08 00:13:07 +0900

    Initial commit

---
+++ src/main/java/hello/embed/EmbedTomcatServletMain.java
@@ -0,0 +1,25 @@
+package hello.embed;
+
+import hello.servlet.HelloServlet;
+import org.apache.catalina.Context;
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.connector.Connector;
+import org.apache.catalina.startup.Tomcat;
+
+public class EmbedTomcatServletMain {
+
+    public static void main(String[] args) throws LifecycleException {
+        System.out.println("EmbedTomcatServletMain.main");
+        //톰캣 설정
+        final Tomcat tomcat = new Tomcat();
+        final Connector connector = new Connector();
+        connector.setPort(8080);
+        tomcat.setConnector(connector);
+
+        // 서블릿 등록
+        final Context context = tomcat.addContext("", "/");
+        tomcat.addServlet("", "helloServlet", new HelloServlet());
+        context.addServletMappingDecoded("/hello-servlet", "helloServlet");
+        tomcat.start();
+    }
+}
---
+++ src/main/java/hello/embed/EmbedTomcatSpringMain.java
@@ -0,0 +1,35 @@
+package hello.embed;
+
+import hello.servlet.HelloServlet;
+import hello.spring.HelloConfig;
+import org.apache.catalina.Context;
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.connector.Connector;
+import org.apache.catalina.startup.Tomcat;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+public class EmbedTomcatSpringMain {
+
+    public static void main(String[] args) throws LifecycleException {
+        System.out.println("EmbedTomcatSpringMain.main");
+        //톰캣 설정
+        final Tomcat tomcat = new Tomcat();
+        final Connector connector = new Connector();
+        connector.setPort(8080);
+        tomcat.setConnector(connector);
+
+        // 스프링 컨테이너 생성
+        final AnnotationConfigWebApplicationContext appContext = new AnnotationConfigWebApplicationContext();
+        appContext.register(HelloConfig.class);
+
+        // 스프링 MVC 디스패처 서블릿 생, 스프링 컨테이너 연결
+        final DispatcherServlet dispatcher = new DispatcherServlet(appContext);
+
+        /// 디스패처 서블릿 등록
+        final Context context = tomcat.addContext("", "/");
+        tomcat.addServlet("", "dispatcher", dispatcher);
+        context.addServletMappingDecoded("/", "dispatcher");
+        tomcat.start();
+    }
+}
